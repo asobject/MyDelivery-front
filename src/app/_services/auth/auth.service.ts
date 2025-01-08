1f6d5b9f648306b7710cbd2bc60fcd9e6ffac6be
x-renegade <alekseymihyalenko@gmail.com>
2025-01-08 17:53:19 +0700

    add auth

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      `${environment.apiUrl}Auth/sign-in`,
+      {
+        username,
+        password,
+      },
+      httpOptions
+    );
+  }
 
-  constructor() { }
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      `${environment.apiUrl}Auth/sign-up`,
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${environment.apiUrl}Auth/signout`, { }, httpOptions);
+  }
+
+  refreshToken() {
+    return this.http.post(`${environment.apiUrl}Auth/refresh-token`, { }, httpOptions);
+  }
 }
